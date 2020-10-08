986bdf9f59df7faa4764a65cc486ca1a67eb27db
Simon Peters <simon.peters@ziggo.nl>
2020-10-08 11:56:51 +0200

    Styled navbar

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -13,17 +13,17 @@ export default class NavigationBar extends React.Component{
         return <div className="navigation-bar">
                 <div className="navigation-icon-box">
                     <img src={homeicon}/>
-                    <span className="icon-box-text"></span>
+                    <span className="icon-box-text pop-r">Home</span>
                 </div>
 
                 <div className="navigation-icon-box">
                     <img src={chaticon}/>
-                    <span className="icon-box-text"></span>
+                    <span className="icon-box-text pop-r">Chats</span>
                 </div>
 
                 <div className="navigation-icon-box">
                     <img src={accounticon}/>
-                    <span className="icon-box-text"></span>
+                    <span className="icon-box-text pop-r">Account</span>
                 </div>
             </div>
     }
